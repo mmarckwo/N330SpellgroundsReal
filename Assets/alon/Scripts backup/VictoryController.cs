235c47aa0c82eb9e6a27db27a7e7878ff7ebf871
psyco doc <thunderfire222@yahoo.com>
2022-03-07 21:48:27 -0500

    backup scripts

---
+++ Assets/alon/Scripts backup/VictoryController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class VictoryController : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        GameObject p = GameObject.FindWithTag("_player");
+        PhotonNetwork.Destroy(p);
+        Invoke("End", 5f);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+             Cursor.lockState = CursorLockMode.None;
+             Cursor.visible = true;
+        }
+
+    }
+
+    public void End()
+    {
+        Application.Quit();
+    }
+}
